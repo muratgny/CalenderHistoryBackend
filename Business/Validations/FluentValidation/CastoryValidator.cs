6acfd00e8a90181b66a5f6678df8b84f9c62a41a
Murat Gunay <42067486+muratgny@users.noreply.github.com>
2023-10-13 12:09:28 +0200

    Basic validation logic defined nad fluent validation packages added

---
+++ Business/Validations/FluentValidation/CastoryValidator.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Validations.FluentValidation
+{
+    public class CastoryValidator : AbstractValidator<Castory>
+    {
+        //All Castory item validation rules comes here
+        public CastoryValidator() 
+        {
+            RuleFor(p => p.Name).MinimumLength(15).MaximumLength(250);
+        }
+    }
+}
